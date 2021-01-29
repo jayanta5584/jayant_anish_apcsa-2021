537bd46509cf15c3bcf1af92fb97d216fb52d994
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-01-28 21:48:27 -0800

    Create AsciiArt.java

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Name: Anish Jayant \n\n" );
+		System.out.println("What type of \\\"ANIMAL\\\" YOU WILL DRAW: Owl" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                ######                 " );
+		System.out.println("              ## .  . ##               " );
+		System.out.println("              ##  >>> ##               " );
+		System.out.println("                ##  ##             " );
+		System.out.println("   ^^^^ ^^^  ##        ##  ^^^ ^^^^   ");
+		System.out.println("    ^^^ ^^ ##            ## ^^ ^^^    ");
+		System.out.println("      ^^ ^ ##            ## ^ ^^      ");
+		System.out.println("        ^^ ##            ## ^^       ");
+		System.out.println("             ##        ##          ");
+		System.out.println("                ##  ##             " );
+		System.out.println("                ||  ||             ");
+		System.out.println("                LL_ LL_            ");
+
+		/*
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+		*/
+	}
+}
