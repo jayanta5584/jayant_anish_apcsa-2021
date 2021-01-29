5da5010af70908f16df5ad5fe8e12a0e4db6820f
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-01-28 21:48:55 -0800

    Create Variables.java

---
+++ Unit00/src/Variables.java
@@ -0,0 +1,81 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Anish Jayant
+//Date - 01/28/2021
+//Class - Mauro CSA P3
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteVar = 127;
+		short shortVar = 320;
+		int intVar = 32;
+		long longVar = 382;
+		float floatVar = -3821;
+		double doubleVar = 32.0;
+		char charVar = 'A';
+		boolean boolVar = false;
+		String stringVar = "hi!!";
+		
+		
+		
+
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Anish Jayant           01/28/21*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteVar = "+byteVar+"\t\t*");
+		System.out.println("*16 bit - shortVar = "+shortVar+"\t\t*");
+		System.out.println("*32 bit - intVar = "+intVar+"\t\t*");
+		System.out.println("*64 bit - longVar = "+longVar+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatVar = "+floatVar+"\t\t*");
+		System.out.println("*64 bit - doubleVar = "+doubleVar+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*     other integer types       *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charVar = "+charVar+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("*boolVar = "+boolVar+"\t\t*");
+		System.out.println("*stringVar = "+stringVar+"\t\t*");
+		
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
