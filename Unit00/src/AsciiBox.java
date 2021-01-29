edfada286a99dd572c067a4ac2721f199903cc85
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-01-28 21:48:44 -0800

    Create AsciiBox.java

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Anish Jayant \t  01/28/21 \n\n" );
+		for (int i = 0; i<3; i++) {
+			for (int j = 0; j<3; j++) {
+				System.out.println("+++++++++++++++++++++++++ " );
+			}
+			for (int k = 0; k<2; k++) {
+				System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+			}
+		}
+		
+
+	}
+}
