b2362cd706e96fb7022bb5548cb1402f3e3a88fc
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-02-02 22:53:15 -0800

    Create Cube.java

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		double sa = 6* Math.pow(side, 2);
+		return sa;
+	}
+}
