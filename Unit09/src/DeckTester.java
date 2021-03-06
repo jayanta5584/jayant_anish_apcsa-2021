0b8a4ad80939dc8f4d0d3b0ff0949bbd73aa7620
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-03-06 13:37:26 -0800

    Update DeckTester.java

--- Unit09/src/DeckTester.java
+++ Unit09/src/DeckTester.java
@@ -13,5 +13,21 @@ public class DeckTester {
 		Deck deck1 = new Deck(ranks1, suits1, values1);
 		System.out.println(deck1);
 		
+		
+		int i = 0; //deal the whole deck
+		while (deck1.size() > 0)
+		{
+			deck1.deal();
+			i++;
+			System.out.println("Deal number: " + i);
+			System.out.println(deck1);
+			assert i + deck1.size() == ranks1.length * suits1.length : "Dealing is out of synch";
+			System.out.println("Shuffling...");
+			deck1.shuffle();
+			System.out.println("Shuffled deck: \n" + deck1.printUndealt());
+			System.out.println();
+		}
+		
+		
 	}
 }
