0b115bae46c84b50aa859627a8b6dec571b57e93
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-03-06 13:37:16 -0800

    Update CardTester.java

--- Unit09/src/CardTester.java
+++ Unit09/src/CardTester.java
@@ -9,5 +9,20 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("King", "Diamond", 12);
+		Card card2 = new Card("Queen", "Spade", 11);
+		Card card3 = new Card("King", "Diamond", 12);
+		
+		//test the accessors
+		System.out.println(card1.rank() + " " + card2.suit() + " " + card3.pointValue());
+		
+		//test the equality function
+		System.out.println(card1.matches(card3));
+		
+		//test the toString function
+		System.out.println(card1.toString() + "\n" + card2.toString() + "\n" + card3.toString());
+		
+		
+		
 	}
 }
