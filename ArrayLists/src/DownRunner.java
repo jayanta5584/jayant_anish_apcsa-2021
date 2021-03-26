e8b4e839052292b24f83911cbb29a3c55a493b61
jayanta5584 <78132823+jayanta5584@users.noreply.github.com>
2021-03-25 23:35:16 -0700

    Create DownRunner.java

---
+++ ArrayLists/src/DownRunner.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class DownRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What size ArrayList would you like?");
+		int arraySize = input.nextInt();
+		List<Integer> arr = new ArrayList<>();
+		for (int i = 0; i<arraySize; i++)
+		{
+			System.out.println("Enter the " + (i+1) + " element");
+			int val = input.nextInt();
+			arr.add(val);
+		}
+		System.out.println(ListDown.go(arr));
+	}
+}
